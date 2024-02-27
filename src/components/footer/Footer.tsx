d72b1c804aa8c8742847178725eefdf263f1c2de
Shresthadev403 <Jagadishdev403@gmail.com>
2024-02-27 18:18:38 +0545

    home page changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
 function Footer() {
-  return <div>footer</div>;
+  return <div></div>;
 }
 
 export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,116 +1,61 @@
+import proileImage from '../assets/profile-no-bg.png';
+import backgroundProfileImage from '../assets/background.png';
+
+import { BsDash } from 'react-icons/bs';
+
 function Home() {
   return (
-    <div>
-      Home Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
-      tempora quas inventore aliquid, quidem atque incidunt provident eligendi
-      officiis expedita, corporis porro consequuntur accusantium suscipit sint
-      quae culpa blanditiis repellendus. Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Illum laudantium quibusdam, reiciendis atque alias
-      eligendi vitae quod pariatur, obcaecati quae laborum a, quis voluptatem
-      fugit non! Veritatis aliquam dolore nemo. Lorem ipsum, dolor sit amet
-      consectetur adipisicing elit. Laboriosam est ullam magnam dolores placeat
-      culpa perferendis non, omnis, quia reiciendis magni repellat architecto
-      cupiditate dolorum vero eligendi eius dolore optio! Alias nesciunt magni
-      in architecto fuga distinctio accusantium praesentium assumenda ab
-      voluptatem? Ex numquam quaerat perspiciatis ab cupiditate eos facere sed
-      reprehenderit, at modi. Ratione omnis dolore minus similique possimus?
-      Incidunt omnis illum inventore accusamus? Molestias, autem tenetur.
-      Voluptate, quo maxime deserunt numquam perferendis ad modi quam
-      consectetur facilis praesentium beatae exercitationem minus! Sapiente,
-      ducimus alias! Officiis tenetur totam soluta. Quis ullam asperiores velit
-      sit quo id debitis deserunt iste odit itaque, ab quam aperiam aliquid est
-      recusandae vero amet nostrum quae hic. Laudantium assumenda placeat quis
-      numquam quod odit. Provident consequatur ab possimus, odit iusto vero
-      blanditiis rerum laborum nihil? Nesciunt iure, ipsa delectus adipisci,
-      cumque sunt porro ab explicabo, modi praesentium magnam distinctio sed
-      debitis deserunt eos ullam? Error necessitatibus reprehenderit quo omnis
-      porro ab sunt illum earum deserunt asperiores, veritatis ipsum commodi
-      temporibus neque consequatur, nostrum dolore nam, fugiat deleniti repellat
-      consequuntur iure. Esse voluptatem harum hic. Assumenda cupiditate libero
-      nam culpa consectetur, placeat error consequuntur maiores ad in eius hic
-      corporis rerum dolorem, excepturi ratione? Odit ipsam suscipit ratione
-      tempora dolorem debitis culpa laboriosam corrupti amet? Eveniet, placeat
-      quasi. Culpa, est. Vel eius eos et maxime at veniam ad eaque expedita,
-      sint qui deleniti voluptatum, quisquam provident eum laudantium commodi
-      soluta inventore quo? Optio, hic sint? Veritatis, molestiae ea laudantium
-      libero sit blanditiis voluptas cum. Qui repellendus voluptate minima
-      labore temporibus asperiores ab recusandae cum repudiandae tenetur,
-      possimus voluptatibus provident expedita. Odit voluptas fuga sunt
-      pariatur. Cumque et quam a neque autem dicta, eum aliquid perspiciatis
-      dolorem minima, temporibus maxime corporis labore quod quae ipsam. Beatae
-      quis qui obcaecati ut architecto optio! Excepturi sequi officiis rerum?
-      Dolore modi aperiam deserunt fugit eaque dicta debitis magni impedit
-      facilis eveniet ex provident labore quaerat, nobis molestiae autem
-      exercitationem magnam accusamus saepe. Dicta, est beatae ipsum libero
-      vitae quia! Possimus sunt animi vitae maiores atque fugit eveniet,
-      mollitia, excepturi exercitationem quos quaerat esse facere itaque
-      consectetur ullam et tempora aspernatur corrupti repudiandae temporibus
-      hic blanditiis optio! Iure, officia beatae? Nemo dolorem ab, vero
-      consequuntur quod sequi necessitatibus ullam nobis rerum quia obcaecati
-      officiis sint suscipit dolor est quo delectus saepe explicabo id odit?
-      Fuga aliquid ea perferendis architecto reprehenderit! Voluptatum velit
-      odit dignissimos odio autem, ea beatae saepe sequi aspernatur at hic
-      laboriosam aperiam vel sapiente et eveniet magnam cumque veritatis porro,
-      perspiciatis quos. Ratione, ut deleniti. Sed, facilis. Mollitia dolorum
-      est adipisci. Minus doloribus eligendi sequi omnis maxime earum voluptate
-      fugiat maiores non quisquam dolorum distinctio, tenetur autem vel
-      architecto iure eveniet mollitia provident nostrum quae. Natus, magnam.
-      Qui assumenda itaque delectus quos sunt illo iusto voluptatum numquam
-      tempora saepe non impedit, animi quo cumque inventore corrupti quae eos,
-      neque fugiat sequi hic deserunt aspernatur blanditiis minus? Maiores!
-      Dolor eveniet quos aperiam laborum ullam iure ex distinctio repellendus.
-      Maiores impedit, maxime nisi earum dolores molestias quaerat culpa sed
-      beatae, fuga, rem atque nihil ex esse magni aliquam dolorem? Pariatur
-      aliquid soluta itaque necessitatibus corporis quae expedita amet ea
-      suscipit eaque omnis ullam architecto similique repudiandae sapiente, id
-      provident perferendis, voluptatibus tenetur. Omnis excepturi vitae
-      sapiente rerum dignissimos assumenda. In animi quod eos molestias?
-      Pariatur a itaque natus eius doloremque, quia, voluptate quibusdam
-      necessitatibus esse tempora sint? Adipisci voluptatem dolor sunt? Natus,
-      necessitatibus! Cupiditate repellat omnis eveniet nisi optio? Ipsa ab
-      delectus molestiae explicabo vero quam nemo nulla error sapiente incidunt
-      veniam assumenda quos, in atque unde ex et aut tenetur architecto? Error
-      totam fugit eius perspiciatis explicabo. Culpa? Error, cumque vero. Ea ad
-      rem, totam corrupti eaque mollitia aspernatur vitae non et temporibus
-      provident laboriosam atque voluptatum eveniet consequuntur dolor
-      consectetur quo quae sequi similique delectus. Laudantium, est! Porro
-      possimus facilis delectus voluptate provident aliquam quam odit quaerat.
-      Facilis doloremque tempore magnam. Saepe, magnam aperiam, corrupti
-      consequuntur repellendus quibusdam ex harum, mollitia earum consequatur
-      eligendi maxime cum doloribus. Voluptatum, velit facere! Id sit molestiae
-      autem architecto dolor modi, enim aut quisquam totam laudantium ipsam nisi
-      similique labore dolores pariatur expedita, obcaecati nobis itaque aperiam
-      suscipit odit culpa amet! Veritatis, quasi deserunt! Recusandae aspernatur
-      laudantium necessitatibus, asperiores itaque ad minus repudiandae ut
-      veritatis quaerat neque, iste a in unde ullam consequuntur, consequatur
-      molestias accusamus voluptate sint totam. Blanditiis, odit? Possimus
-      delectus id porro corporis sequi nihil, rerum optio fugiat esse ipsam
-      architecto culpa aperiam provident minima obcaecati? Praesentium voluptate
-      repellat tempore quo pariatur ullam quidem veritatis perferendis in saepe!
-      Cum explicabo enim dolore dicta doloremque, laboriosam ex numquam, nam
-      expedita, dolor quisquam quia error excepturi eos? Fugiat architecto
-      cumque eius ea harum repudiandae mollitia! Accusantium non earum repellat
-      officia! Illo error expedita velit hic dolore totam, itaque vel molestias
-      ipsam doloremque quo nesciunt cum quas laborum a, praesentium, temporibus
-      minus laboriosam quidem exercitationem obcaecati omnis veritatis odit.
-      Ratione, nobis! Laudantium perferendis omnis iusto cupiditate blanditiis
-      quas, atque repellendus suscipit! Fuga vitae aut nobis quasi magnam
-      laboriosam voluptatem dolor ullam iusto repellat veniam culpa non quae
-      expedita, quod nemo tempore! Error sit magnam dicta dolorem eum suscipit
-      voluptatum blanditiis illo incidunt beatae totam, consequatur
-      exercitationem ex temporibus eligendi, quibusdam voluptatem. Et, dolor
-      minima quia harum libero nihil distinctio enim voluptate? Natus, ratione,
-      facilis molestias expedita repellat soluta, deleniti tempore tenetur atque
-      sequi hic non nobis modi. Eum voluptatibus voluptates, ratione facilis
-      quam iure, reiciendis delectus molestiae atque sequi, nihil tenetur.
-      Explicabo facilis quod quidem nemo laudantium doloribus, eaque cumque
-      provident ullam omnis rerum velit illo et debitis adipisci. Repudiandae
-      est tempore recusandae, vel debitis laboriosam nisi libero architecto
-      incidunt molestiae? Sunt ullam nesciunt tempore eaque quo eius, dolorum
-      iure qui quam nobis. Qui, cupiditate quo quas non est dolores temporibus
-      reprehenderit numquam, ipsum quisquam blanditiis ipsa ut similique
-      eveniet? Est?
-    </div>
+    <>
+      <div className="flex h-96 flex-col md:flex-row">
+        {/* introduction part */}
+
+        <div className="w-full md:w-1/2 flex flex-col gap-1 justify-center ml-0 p-2 md:ml-24">
+          <h2 className="w-full text-xl font-medium flex items-center ">
+            {' '}
+            <BsDash /> Hello
+          </h2>
+          <p className="w-full font-medium text-2xl">
+            I am{' '}
+            <span className="w-full text-red-munsell font-semibold text-2xl ">
+              Jagadish Shrestha
+            </span>
+          </p>
+          <p className="w-full text-sm ">
+            Jagadish Shrestha a seasoned software developer renowned for his
+            proficiency in backend development, frontend design, and DevOps
+            practices, ensuring seamless integration and optimal performance
+            across the full software development lifecycle.
+          </p>
+          <div className="mt-10">
+            <button className="bg-red-munsell text-ghost-white px-2 py-1">
+              DOWNLOAD CV
+            </button>
+          </div>
+        </div>
+        <div className="md:w-1/2 flex justify-center  ">
+          <div
+            className=""
+            style={{
+              maxWidth: '600px',
+              maxHeight: '400px',
+              backgroundImage: `url(${backgroundProfileImage})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              height: '100%',
+              width: '100%',
+              margin: '1rem',
+            }}
+          >
+            {/* sssssssssssssssssssssssssssss */}
+            <img
+              src={proileImage}
+              alt="jagadish shrestha"
+              className=" h-full w-full  object-cover "
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
